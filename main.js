7f81745736a6077cb534a79122404d7ae9488ab4
NikhilreddyBurra <nikhilburra402@gmail.com>
2022-01-24 13:06:14 +0530

    Added different buttons

--- main.js
+++ main.js
@@ -6,13 +6,32 @@ function addItem() {
     li.appendChild(document.createTextNode(candidate.value));
 
     var span = document.createElement("span");
-    var txt = document.createTextNode("\u00D7");
+    var span2 = document.createElement("span2")
+    var span3 = document.createElement("span3")
+    var txt = document.createTextNode("\u2713");
+    var undo = document.createTextNode("\u238C");
+    var del = document.createTextNode("\u00D7");
     span.className = "close";
+    span2.className = "close2";
+    span3.className = "close3";
+    inputField.value = "";
     span.appendChild(txt);
+    span2.appendChild(undo);
+    span3.appendChild(del);
     span.addEventListener('click', () => {
-        li.remove();
+        li.style.textDecorationLine = "line-through";
+        // span.innerText = "\u00D7"
+        
     });
+    span2.addEventListener('click', () => {
+        li.style.textDecorationLine ="none";
+    });
+    span3.addEventListener('click', () => {
+        li.remove();
+    })
 
     li.appendChild(span);
+    li.appendChild(span2);
+    li.appendChild(span3);
     ul.appendChild(li);
 }
